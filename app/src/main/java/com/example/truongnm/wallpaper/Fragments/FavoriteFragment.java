56c7c1713825c8a60c76d0493db6f22f6a42e968
TruongNM <manhtruonghedspi@gmail.com>
2018-07-08 16:38:09 +0700

    Create Main Fragments

---
+++ app/src/main/java/com/example/truongnm/wallpaper/Fragments/FavoriteFragment.java
@@ -0,0 +1,28 @@
+package com.example.truongnm.wallpaper.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.truongnm.wallpaper.R;
+
+/**
+ * Created by truongnm on 7/8/18.
+ */
+
+public class FavoriteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
---
+++ app/src/main/java/com/example/truongnm/wallpaper/Utils/Functions.java
@@ -0,0 +1,19 @@
+package com.example.truongnm.wallpaper.Utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.truongnm.wallpaper.R;
+
+/**
+ * Created by truongnm on 7/8/18.
+ */
+
+public class Functions {
+    public static void changeMainFragment(FragmentActivity fragmentActivity, Fragment fragment){
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit();
+    }
+}
