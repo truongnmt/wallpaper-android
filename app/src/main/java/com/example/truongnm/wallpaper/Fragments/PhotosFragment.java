0d41edd14fc376118dc4ada8724b8983a61f93a8
Truong Nguyen <manhtruonghedspi@gmail.com>
2018-07-10 16:58:01 +0700

    Get photos from Unsplash

--- app/src/main/java/com/example/truongnm/wallpaper/Fragments/PhotosFragment.java
+++ app/src/main/java/com/example/truongnm/wallpaper/Fragments/PhotosFragment.java
@@ -3,21 +3,50 @@ package com.example.truongnm.wallpaper.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.example.truongnm.wallpaper.Adapters.PhotosAdapter;
+import com.example.truongnm.wallpaper.Models.Photo;
 import com.example.truongnm.wallpaper.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by truongnm on 7/8/18.
  */
 
 public class PhotosFragment extends Fragment {
+    private final String TAG = PhotosFragment.class.getSimpleName();
+
+    @BindView(R.id.fragment_photos_progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.fragment_photos_recyclerview)
+    RecyclerView recyclerView;
+
+    private PhotosAdapter photosAdapter;
+    private List<Photo> photos = new ArrayList<>();
+
+    private Unbinder unbinder;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photos, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        // RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        photosAdapter = new PhotosAdapter(getActivity(), photos);
         return view;
     }
 
--- app/src/main/java/com/example/truongnm/wallpaper/Webservices/ApiInterface.java
+++ app/src/main/java/com/example/truongnm/wallpaper/Webservices/ApiInterface.java
@@ -1,4 +1,13 @@
 package com.example.truongnm.wallpaper.Webservices;
 
+import com.example.truongnm.wallpaper.Models.Photo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+    @GET("photos")
+    Call<List<Photo>> getPhotos();
 }
