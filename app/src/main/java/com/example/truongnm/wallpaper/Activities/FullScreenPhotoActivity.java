88374b3b94e0dce294ba2a0b9ad8fea24564c275
Truong Nguyen <manhtruonghedspi@gmail.com>
2018-07-12 11:00:43 +0700

    Get photo from Unsplash by ID

--- app/src/main/java/com/example/truongnm/wallpaper/Activities/FullScreenPhotoActivity.java
+++ app/src/main/java/com/example/truongnm/wallpaper/Activities/FullScreenPhotoActivity.java
@@ -1,14 +1,22 @@
 package com.example.truongnm.wallpaper.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.example.truongnm.wallpaper.Adapters.GlideApp;
+import com.example.truongnm.wallpaper.Models.Photo;
 import com.example.truongnm.wallpaper.R;
+import com.example.truongnm.wallpaper.Webservices.ApiInterface;
+import com.example.truongnm.wallpaper.Webservices.ServiceGenerator;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
@@ -17,10 +25,13 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FullScreenPhotoActivity extends AppCompatActivity {
     @BindView(R.id.activity_fullscreen_photo_photo)
-    ImageView photo;
+    ImageView fullscreenPhoto;
     @BindView(R.id.activity_fullscreen_photo_user_avatar)
     CircleImageView userAvatar;
     @BindView(R.id.activity_fullscreen_photo_fab_menu)
@@ -46,7 +57,44 @@ public class FullScreenPhotoActivity extends AppCompatActivity {
     }
 
     private void getPhoto(String id){
-        
+        ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
+        Call<Photo> call = apiInterface.getPhoto(id);
+        call.enqueue(new Callback<Photo>() {
+            @Override
+            public void onResponse(Call<Photo> call, Response<Photo> response) {
+                if(response.isSuccessful()){
+                    Photo photo = response.body();
+                    updateUI(photo);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Photo> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void updateUI(Photo photo){
+        try{
+            username.setText(photo.getUser().getUsername());
+            GlideApp.with(FullScreenPhotoActivity.this)
+                    .load(photo.getUser().getProfileImage().getSmall())
+                    .into(userAvatar);
+
+            GlideApp.with(FullScreenPhotoActivity.this)
+                    .asBitmap()
+                    .load(photo.getUrl().getRegular())
+                    .centerCrop()
+                    .into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            fullscreenPhoto.setImageBitmap(resource);
+                        }
+                    });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @OnClick(R.id.activity_fullscreen_photo_fab_favorite)
